1027393823c3ef2713d268a079c3f5d05e1993ed
Mao, Weicheng <weicheng.mao@gmail.com>
2017-11-18 18:58:02 +0800

    remove unnecessary part.
    Change-Id: Ia436bd8c9c776a44a295da602770649d4e23f399
    Signed-off-by: Mao, Weicheng <weicheng.mao@gmail.com>

--- src/main/java/mobi/dashuxia/configuration/mybatis/MybatisConfig.java
+++ src/main/java/mobi/dashuxia/configuration/mybatis/MybatisConfig.java
@@ -1,40 +1,84 @@
 package mobi.dashuxia.configuration.mybatis;
 
-import java.io.IOException;
+import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.StandardEnvironment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.util.ClassUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.github.pagehelper.PageHelper;
 
 import mobi.dashuxia.configuration.JdbcConnectionSettings;
 
 @Configuration
+@EnableTransactionManagement
 @MapperScan(basePackages = "mobi.dashuxia.mapper")
-public class MybatisConfig {
+public class MybatisConfig implements TransactionManagementConfigurer{
 
-	private static Logger log = LoggerFactory.getLogger(MybatisConfig.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(MybatisConfig.class);
 
 	@Autowired
 	private JdbcConnectionSettings jdbcConnectionSettings;
 
+    @Autowired
+    DataSource dataSource;
+    
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactoryBean() {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setTypeAliasesPackage("mobi.dashuxia.domain");
 
+        //分页插件
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("supportMethodsArguments", "true");
+        properties.setProperty("returnPageInfo", "check");
+        properties.setProperty("params", "count=countSql");
+        pageHelper.setProperties(properties);
+
+        //添加插件
+        bean.setPlugins(new Interceptor[]{pageHelper});
+
+        //添加XML目录
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        try {
+            bean.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+            return bean.getObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
 	@Bean
 	public DataSource dataSource() {
 		DruidDataSource ds = new DruidDataSource();
@@ -52,39 +96,14 @@ public class MybatisConfig {
 		return ds;
 	}
 
-
-	public Resource[] getResource( String basePackage, String pattern ) throws IOException {
-		String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
-				+ ClassUtils.convertClassNameToResourcePath(new StandardEnvironment()
-						.resolveRequiredPlaceholders(basePackage)) + "/" + pattern;
-		Resource[] resources = new PathMatchingResourcePatternResolver().getResources(packageSearchPath);
-		return resources;
-	}
-
-
-	@Bean
-	public SqlSessionFactory sqlSessionFactory() throws Exception {
-		log.debug("> sqlSessionFactory");
-		final SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-		sqlSessionFactory.setDataSource(dataSource());
-		sqlSessionFactory.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
-		sqlSessionFactory.setFailFast(true);
-		sqlSessionFactory.setMapperLocations(getResource("mapper", "**/*.xml"));
-        sqlSessionFactory.setTypeHandlersPackage(
-                "mobi.dashuxia.configuration.mybatis.typehandler");
-		return sqlSessionFactory.getObject();
-	}
-
-
 	@Bean
 	public DataSourceTransactionManager transactionManager() {
-		log.debug("> transactionManager");
+	    LOGGER.debug("> transactionManager");
 		return new DataSourceTransactionManager(dataSource());
 	}
 
-
 	@PostConstruct
 	public void postConstruct() {
-		log.info("jdbc.settings={}", jdbcConnectionSettings);
+	    LOGGER.debug("jdbc.settings={}", jdbcConnectionSettings);
 	}
 }
